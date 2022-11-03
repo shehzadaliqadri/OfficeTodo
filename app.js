756bd89c965b1d784a930e52cf7148e2b6e5e8cd
Shehzad Ali Qadri <h.shehzad.ali@gmail.com>
2022-11-03 21:57:20 +0000

    upload files feature added

--- app.js
+++ app.js
@@ -13,6 +13,12 @@ import {
   setDoc
 } from "https://www.gstatic.com/firebasejs/9.11.0/firebase-firestore.js";
 
+import {
+  getStorage,
+  ref,
+  uploadBytesResumable
+} from "https://www.gstatic.com/firebasejs/9.11.0/firebase-storage.js";
+
 
 const firebaseConfig = {
   apiKey: "AIzaSyBxUgfFLuGpK7xTs54jlEXog0CHLTiaUlA",
@@ -26,7 +32,10 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig),
   auth = getAuth(),
   user = auth.currentUser,
-  loginBtn = document.getElementById("signinBtn"),
+  db = getFirestore(app),
+  storage = getStorage(app);
+
+const loginBtn = document.getElementById("signinBtn"),
   loginEmail = document.getElementById("signinEmail"),
   loginPassword = document.getElementById("signinPassword"),
   signupBtn = document.getElementById("signupBtn"),
@@ -34,14 +43,17 @@ const app = initializeApp(firebaseConfig),
   signupPassword = document.getElementById("signupPassword"),
   signupName = document.getElementById("signupName"),
   main = document.getElementById("main"),
-  db = getFirestore(app);
+  file = document.getElementById("file");
+
 let redirectTime = 10000;
 
 if (signupBtn) {
   signupBtn.addEventListener("click", () => {
     console.log(signupEmail.value, "input email")
     console.log(signupPassword.value, "input password")
-
+console.log(file.files[0])
+console.log(file.files[0].type)
+    
     createUserWithEmailAndPassword(auth, signupEmail.value, signupPassword.value)
       .then(async (userCredential) => {
         const user = userCredential.user;
@@ -105,5 +117,65 @@ window.onload = async () => {
       // ...
     }
   });
+}
+
+const uploadFile = (file,uid) =>{
+return new Promise(resolve, reject) => {
+  const storage = getStorage();
+
+// Upload file and metadata to the object 'images/mountains.jpg'
+const storageRef = ref(storage, 'images/' + file.name);
+const uploadTask = uploadBytesResumable(storageRef, file, metadata);
+
+// Listen for state changes, errors, and completion of the upload.
+uploadTask.on('state_changed',
+  (snapshot) => {
+    // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+    const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+    console.log('Upload is ' + progress + '% done');
+    switch (snapshot.state) {
+      case 'paused':
+        console.log('Upload is paused');
+        break;
+        
+      case 'running':
+        console.log('Upload is running');
+        break;
+    }
+  }, 
+  (error) => {
+    // A full list of error codes is available at
+    // https://firebase.google.com/docs/storage/web/handle-errors
+    switch (error.code) {
+      case 'storage/unauthorized':
+        // User doesn't have permission to access the object
+    reject(error)
+        break;
+      case 'storage/canceled':
+        // User canceled the upload
+    reject(error)
+        break;
+
+      // ...
+
+      case 'storage/unknown':
+        // Unknown error occurred, inspect error.serverResponse
+    reject(error)
+        
+        break;
+    }
+    reject(error)
+  }, 
+  () => {
+    // Upload completed successfully, now we can get the download URL
+    getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+      console.log('File available at', downloadURL);
+      resolve(downloadURL)
+      
+    });
+  }
+);
+}
+  
 }
 
